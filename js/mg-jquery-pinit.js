e448d6c6f5ea73876f9da6aec44f050191699f56
mgiulio <giulio.mainardi@gmail.com>
2012-03-14 09:45:58 +0100

    Turn it into a jQuery plugin

---
+++ js/mg-jquery-pinit.js
@@ -0,0 +1,47 @@
+(function($){
+
+var pics;
+
+$.fn.pinit = function() {
+	pics = this;
+	$(window).load(run);
+	return this;
+};
+
+function run() {
+	var pageUrl = window.location.href;
+	
+	pics.each(function() {
+		var
+			iframeUrl = window.location.protocol + '//d3io1k5o0zdpqr.cloudfront.net/pinit.html'
+			qs = $.param({
+				url: pageUrl,
+				media: 'http://mgiulio.altervista.org/wp-content/uploads/2012/01/Playing-with-Open-Data-a-simple-viewer-for-DecoroUrbano.org_.jpg',//this.src,
+				description: this.title,
+				layout: 'none'
+			}),
+			iframe = $('<iframe></iframe>')
+				.attr('src', iframeUrl + '?' + qs)
+				.attr('scrolling', 'no')
+				.attr('allowtransparency', true)
+				.attr('frameborder', 0)
+				.css({
+					position: 'absolute',
+					right: '2px',
+					top: '2px',
+					border: 'none',
+					width: '43px',
+					height: '20px'
+				})
+		;
+		$(this)
+			.wrap(
+				$('<div class="mg-container" style="position: relative;"></div>')
+					.css({width: $(this).width(), height: $(this).height()})
+			)
+			.after(iframe)
+		;
+	});
+}
+
+})(jQuery);
